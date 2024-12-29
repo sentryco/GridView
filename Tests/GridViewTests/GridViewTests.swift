d5c8689d507bd31af041f19744761d3e009b7dea
eonist <30n1st@gmail.com>
2024-12-29 15:43:01 +0100

    Remove swift 6.0 UITest that breaks github actions

--- Tests/GridViewTests/GridViewTests.swift
+++ Tests/GridViewTests/GridViewTests.swift
@@ -1,6 +1,10 @@
-import Testing
+//import Testing
 @testable import GridView
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+func testingExample() {
+   let _ = {}()
 }
+
+//@Test func example() async throws {
+//    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//}
